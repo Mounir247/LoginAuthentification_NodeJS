f8ce37fb131d0b9257b160b54c52305bb89060a2
Mounir Laskri <laskri.mounir@gmail.com>
2020-02-05 23:23:01 +0100

    Port Server test

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const PORT = process.env.PORT || 5000;
 const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
 const mongoose = require('mongoose');
@@ -54,6 +55,6 @@ app.use(
 //Routes
 app.use('/', require('./routes/index'));
 app.use('/users', require('./routes/users'));
-const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, console.log(`server started on port ${PORT}`));
 
